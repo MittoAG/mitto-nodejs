cdb92f1bc7110c6e90564f70ab6692273718f49e
Ramon Kania <ramon@mitto.ch>
2019-06-02 14:55:59 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# mitto-nodejs
-The official Node.js client for Mitto's REST API.
+# Mitto REST API Node.js client
+
+This is the official Node.js client for the Mitto REST API (<https://www.mitto.ch>).
+
+NPM-Package: <https://www.npmjs.com/package/mitto-rest-client>
+
+## Install
+
+```Console
+npm install mitto-rest-client
+```
+
+## Usage
+
+### Simple SMS sending
+
+```javascript
+var apiKey = "YOUR-API-KEY";
+var testMode = false; // optional; set to true if you want to send a test messages (no delivery, no cost, but visible in your portal)
+
+require("mitto-rest-client")
+    .create(apiKey, testMode)
+    .sendTextMessage("YourSender", "491771234567", "Hello World!");
+```
+
+### Send flash SMS
+
+```javascript
+var apiKey = "YOUR-API-KEY";
+
+require("mitto-rest-client")
+    .create(apiKey)
+    .sendTextMessage("YourSender", "491771234567", "Hello World!", {flash: true});
+```
+
+### Set a client reference
+
+```javascript
+var apiKey = "YOUR-API-KEY";
+
+require("mitto-rest-client")
+    .create(apiKey)
+    .sendTextMessage("YourSender", "491771234567", "Hello World!", {reference: "My internal reference number #1"});
+
+// in the Mitto customer portal you can configure a DeliveryReport Callback URL; this will receive the "reference" parameter from above...
+// alternatively you can support your Account Manager to set the URL for you.
+```
+
+### Unicode SMS sending
+
+```javascript
+var apiKey = "YOUR-API-KEY";
+
+require("mitto-rest-client")
+    .create(apiKey)
+    .sendUnicodeTextMessage("YourSender", "491771234567", "привет");
+```
+
+### Inspect response
+
+```javascript
+var apiKey = "YOUR-API-KEY";
+
+require("mitto-rest-client")
+    .create(apiKey)
+    .sendTextMessage(
+        "YourSender",
+        "491771234567",
+        "Hello World!",
+        function(error, apiResponse) {
+            if(error) {
+                console.error(error);
+            }
+            if(apiResponse) {
+                console.log(
+                    "ResponseCode: " + apiResponse.responseCode + "\n" +
+                    "ResponseText: " + apiResponse.responseText + "\n" +
+                    "MessageId: " + apiResponse.id + "\n" +
+                    "Timestamp: " + apiResponse.timestamp + "\n" +
+                    "TextLength: " + apiResponse.textLength + "\n\n"
+                );
+            }
+        }
+    );
+```
---
+++ index.js
@@ -0,0 +1,121 @@
+var https = require("https");
+
+String.prototype.lpad = function(padString, length) {
+	var str = this;
+	while (str.length < length)
+		str = padString + str;
+	return str;
+};
+
+var MittoClient = function(apiKey, testMode, debugMode){
+    var headers = {
+		"Content-Type": "application/json",
+		"User-Agent": "Mitto/Client/Node.js/2.0",
+        "X-Mitto-API-Key": apiKey
+    };
+	var baseParams = {};
+	if(testMode && testMode == true) {
+		baseParams.test = true;
+	}
+	var sendSmsPath = "/sms.json";
+	
+	function log(logMessage) {
+		if (logMessage instanceof Error) console.log(logMessage.stack);
+		if (debugMode) {
+			if (typeof logMessage === "object") {
+				console.dir(logMessage);
+			} else {
+				console.log(logMessage);
+			}
+		}
+	}
+	
+	function sendRequest(path, method, postData, callback) {
+		var responseCallback = function(response) {
+			var responseReturn = "";
+			response.setEncoding("utf8"); 
+			response.on("data", function(chunk) {
+        		responseReturn += chunk;
+			});			
+			response.on("end", function() { 
+				log("response ended");
+				if (callback) {
+					var jsonResponse = responseReturn;
+					var error = null;
+					try {
+					jsonResponse = JSON.parse(responseReturn);
+					} catch (parseException) {
+						log(parseException);
+						log("Response wasn't proper JSON, returning raw response to client");
+						error = parseException;
+					}
+					callback(error, jsonResponse);
+				}
+			});
+		};
+        
+        var postDataJSON = JSON.stringify({...postData, ...baseParams});
+        var options = {
+			host: "rest.mittoapi.com",
+    		port: 443,
+    		path: path,
+    		method: method,
+            headers: {...headers, "Content-Length": Buffer.byteLength(postDataJSON)}
+        };
+        
+		log("executing " + options.method + " request to " + options.host + ":" + options.port + options.path + " POST-Data: " + postDataJSON);
+        
+        var request = https.request(options, responseCallback);
+        
+        request.write(postDataJSON);
+		request.end();
+		
+		request.on("error", function(e) {
+			log("error on API request; detailed stacktrace below ");
+			log(e);
+			callback(e);
+		});
+	}
+	
+	function sendMessage(message, additionalOptions, callback) {
+		sendRequest(sendSmsPath, "POST", {...message, ...additionalOptions}, function(error, apiResponse) {
+			if (!error && apiResponse && apiResponse.responseCode && apiResponse.responseCode > 0) {
+				tryHandleError(callback, new Error("Mitto REST API returned ResponseCode " + apiResponse.responseCode + " (" + apiResponse.responseText + ")"), apiResponse);
+			} else if (callback) {
+				callback(error, apiResponse);
+			}
+		});
+	}
+	
+	function tryHandleError(callback, error, returnData) {
+		if (callback) {
+			callback(error, returnData);
+		} else {
+			throw error;
+		}
+	}
+	
+	function sendTextMessage(from, to, text, additionalOptions, callback) {
+		sendMessage({from: from, to: to, text: text}, additionalOptions, callback)
+	}
+	
+	function sendUnicodeTextMessage(from, to, text, additionalOptions, callback) {
+		var unicodeDigits = stringToUnicodeCodePoints(text);
+		sendMessage({from: from, to: to, text: unicodeDigits, type: "unicode"}, additionalOptions, callback)
+	}
+	
+	function stringToUnicodeCodePoints(text) {
+		var codePoints = "";
+		for (var i = 0, length = text.length; i < length; i++) {
+  			codePoints = codePoints + text.codePointAt(i).toString(16).lpad("0", 4);
+		}
+		return codePoints.toUpperCase();
+	}
+	
+	return {
+		sendTextMessage: sendTextMessage,
+		sendUnicodeTextMessage: sendUnicodeTextMessage
+	};
+};
+
+exports.create = MittoClient;
